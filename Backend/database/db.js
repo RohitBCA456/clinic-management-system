3e19865e9bee67033c276109f302a8bb8934bfb8
RohitBCA456 <ry2281104@gmail.com>
2025-07-25 14:14:00 +0530

    fix

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}`
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
     );
     console.log(
       `Connected to database host: ${connectionInstance.connection.host}`
